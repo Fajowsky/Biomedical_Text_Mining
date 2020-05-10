57fc903d501a637ed9dcad24022910366a1d92fb
vectorkt <30572245+vectorkt@users.noreply.github.com>
2020-05-10 13:36:41 +0100

    Add files via upload

---
+++ Code/02Website/01 Database and Network Graph/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Protein(models.Model):
+
+    name = models.CharField(max_length=64)
+    interactions = models.ManyToManyField("self", symmetrical=True, through="Interaction")
+
+    def __str__(self):
+        return self.name
+
+
+class Interaction(models.Model):
+
+    frm = models.ForeignKey(
+        Protein, on_delete=models.CASCADE, related_name="interactions_to"
+    )
+    to = models.ForeignKey(
+        Protein, on_delete=models.CASCADE, related_name="interactions_from"
+    )
+    publication = models.CharField(max_length=64)
+    
+    abstract = models.CharField(max_length=64)
+    
+    link = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.publication}: {self.frm} <-> {self.to}"
+        
+        
